65bfdab7716a5d9d51811bb89da419838154b85f
franUNC <francisco.cabrera@mi.unc.edu.ar>
2022-04-01 18:30:13 -0300

    Rompi el programa para probar el bitrise

--- hellowolrdtestapp/app/src/main/java/org/teamrocketunc/hello_world_test_app/MainActivity.java
+++ hellowolrdtestapp/app/src/main/java/org/teamrocketunc/hello_world_test_app/MainActivity.java
@@ -25,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void openActivity2(){
-        Intent intent = new Intent(this, Activity2.class);
+        Intent intent = new Intentt(this, Activity2.class);
         startActivity(intent);
     }
 }
